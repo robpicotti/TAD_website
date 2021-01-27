e052eda382a258a91fc97bc44b8e3ff05672565a
rob@biza.co.za <robpicotti@gmail.com>
2021-01-27 14:12:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace TAD_website
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "cmd.exe";
+            info.RedirectStandardInput = true;
+            info.UseShellExecute = false;
+            p.StartInfo = info;
+            p.Start();
+            using (StreamWriter sw = p.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    sw.WriteLine("conda activate website_prod");
+                    sw.WriteLine("flask run");
+                }
+            }
+        }
+    }
+}
